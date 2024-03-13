e3f6972a1980f1896c2105a07d00c7395e90668f
indraBandara <61151535+indraBandara@users.noreply.github.com>
2024-03-13 10:20:05 +0530

    comm

---
+++ src/main/java/com/navtsa/promender/ProMenderApplication.java
@@ -0,0 +1,13 @@
+package com.navtsa.promender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProMenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProMenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navtsa/promender/ProMenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navtsa.promender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProMenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
